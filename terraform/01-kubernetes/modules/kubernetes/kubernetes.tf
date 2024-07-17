7ebc935b37573ad6d525873561ef462e781038e5
Cameron Jeffries <camservo@gmail.com>
2024-07-17 11:34:16 -0700

    Initial commit

---
+++ terraform/01-kubernetes/modules/kubernetes/kubernetes.tf
@@ -0,0 +1,13 @@
+resource "linode_lke_cluster" "kubernetes" {
+  k8s_version = var.k8s_version
+  label       = "kubernetes-${var.environment}"
+  region      = var.region
+  tags        = var.tags
+  dynamic "pool" {
+    for_each = var.pools
+    content {
+      type  = pool.value["type"]
+      count = pool.value["count"]
+    }
+  }
+}
