497802f3c4230aa55b5e2a7782c40901a41d9d69
Cameron Jeffries <camservo@gmail.com>
2024-07-30 17:31:19 -0700

    adding vault helm

--- terraform/01-kubernetes/main.tf
+++ terraform/01-kubernetes/main.tf
@@ -8,22 +8,3 @@ module "kubernetes" {
     }
   ]
 }
-resource "kubernetes_namespace" "example" {
-  metadata {
-    annotations = {
-      name = "example-annotation"
-    }
-
-    labels = {
-      mylabel = "label-value"
-    }
-
-    name = "terraform-example-namespace"
-  }
-}
-resource "helm_release" "ahoy" {
-  name       = "ahoy"
-  namespace  = kubernetes_namespace.example.id
-  repository = "https://helm.github.io/examples"
-  chart      = "hello-world"
-}
