09276c86718a844a5c97d4834b4773ccf75f79fd
Cameron Jeffries <camservo@gmail.com>
2025-02-12 18:46:43 -0800

    adding vault operator

--- terraform/01-kubernetes/main.tf
+++ terraform/01-kubernetes/main.tf
@@ -18,22 +18,3 @@ module "argocd" {
 module "keycloak" {
   source = "./modules/keycloak"
 }
-# module "keycloak_shiraj" {
-#   source      = "./modules/keycloak"
-#   namespace   = "keycloak-shiraj"
-#   environment = "shiraj"
-# }
-# module "monitoring" {
-#   source = "./modules/monitoring"
-# }
-
-# resource "github_actions_organization_secret" "keycloak_user" {
-#   secret_name     = "KEYCLOAK_ADMIN_USER"
-#   visibility      = "private"
-#   plaintext_value = module.keycloak.keycloak_user
-# }
-# resource "github_actions_organization_secret" "keycloak_pass" {
-#   secret_name     = "KEYCLOAK_ADMIN_PASS"
-#   visibility      = "private"
-#   plaintext_value = module.keycloak.keycloak_pass
-# }
--- terraform/01-kubernetes/modules/vault/main.tf
+++ terraform/01-kubernetes/modules/vault/main.tf
@@ -12,3 +12,20 @@ resource "helm_release" "vault" {
     file("${path.module}/values.yaml")
   ]
 }
+## Haven't gotten this one working yet.
+resource "helm_release" "vault_secrets_operator" {
+  name = "helm-secrets-operator"
+  namespace = kubernetes_namespace.vault.id
+  repository = "hashicorp"
+  chart = "vault-secrets-operator"
+  values = [
+    file("${path.module}/vault-operator-values.yaml")
+  ]
+}
+# helm install vault-secrets-operator hashicorp/vault-secrets-operator -n vault-secrets-operator-system --create-namespace --values vault/vault-operator-values.yaml
+resource "kubernetes_service_account" "vault" {
+  metadata {
+    name = "k8s-service-account"
+    namespace = "backstage"
+  }
+}
